8cba0f59aa94c6f5c3f65fc28e117c240aa59766
Christian Hotte <christianwhotte@gmail.com>
2022-10-18 21:15:45 -0400

    VR Player Quality-of-Life
    -Reworked hand collision system
    -Hand lerp

--- Assets/3_Prefabs/VRPlayer/VRHand/VRHandController.cs
+++ Assets/3_Prefabs/VRPlayer/VRHand/VRHandController.cs
@@ -17,6 +17,7 @@ public class VRHandController : MonoBehaviour
 
     //Settings:
     [Header("Gamefeel Settings:")]
+    [SerializeField, Tooltip("Moves where on controller hands stick to")]                                     private Vector3 followOffset;
     [Min(0), SerializeField, Tooltip("Maximum speed at which hand can move")]                                 private float maxSpeed;
     [Min(0), SerializeField, Tooltip("Rate at which hand seeks target position")]                             private float linearFollowSpeed;
     [Min(0), SerializeField, Tooltip("Rate at which hand seeks target rotation")]                             private float angularFollowSpeed;
@@ -24,9 +25,10 @@ public class VRHandController : MonoBehaviour
     [Header("Physics Settings:")]
     [Min(0), SerializeField, Tooltip("Size of main hand collider")]                                                 private float palmCollisionRadius;
     [Min(0), SerializeField, Tooltip("Radius of fingertip colliders")]                                              private float fingerTipRadius;
-    [SerializeField, Tooltip("Difference between root position of hand and position of palm collider")]             private Vector3 palmColliderOffset;
+    [Min(0), SerializeField, Tooltip("Maximum depth inside colliders which finger targets can penetrate")]          private float maxProjectionDepth;
     [SerializeField, Tooltip("Physics layers which fingers on hand are able to collide with")]                      private LayerMask obstructionLayers;
     [Min(1), SerializeField, Tooltip("Maximum number of obstructions hand and fingers can collide with per frame")] private int maxObstacleCollisions = 1;
+    public AnimationCurve speedCurve;
     //[SerializeField, Tooltip("Enables player to lift hands out of bodies when fingers are trapped underneath them (0-90)")] private float minRaiseReleaseAngle;
 
     //Runtime Variables:
@@ -35,6 +37,7 @@ public class VRHandController : MonoBehaviour
     /// </summary>
     internal HandType side = HandType.None; //Initialize at "None" to indicate hand has not yet been associated with player controller
     private Vector3 velocity;               //Last recorded linear velocity of this seeker object
+    private Vector3 prevTargetPosition;     //Last recorded position of controller target
 
     //RUNTIME METHODS:
     private void Awake()
@@ -73,47 +76,57 @@ public class VRHandController : MonoBehaviour
     private void Start()
     {
         //Initialize:
-        if (controllerTarget != null) obstructedTarget.position = controllerTarget.position; //Set target reference position if possible
+        if (controllerTarget != null)
+        {
+            obstructedTarget.position = controllerTarget.position; //Set target reference position if possible
+            prevTargetPosition = controllerTarget.position;
+        }
+            
+        if (side == HandType.Right)
+        {
+            followOffset.x *= -1;
+        }
     }
     private void Update()
     {
         //Perform positional update:
         if (controllerTarget != null && obstructedTarget != null) //Both targeting references need to be present for system to function
         {
-            transform.rotation = Quaternion.Slerp(transform.rotation, controllerTarget.rotation, angularFollowSpeed * Time.deltaTime); //TEMP set rotation
-
             //Initialize:
             float scaleMultiplier = 1; if (VRPlayerController.main != null) scaleMultiplier = VRPlayerController.main.transform.localScale.x; //Get multiplier for maintaining properties despite changes in scaling
             Vector3 prevPosition = transform.position; //Get position before all checks
+            Vector3 offsetTargetPos = controllerTarget.position + controllerTarget.TransformVector(followOffset);
 
-            //Move toward target position:
-            Vector3 newPosition = transform.position;                                                                      //Get current position as modifiable variable
-            newPosition = Vector3.Lerp(transform.position, controllerTarget.position, linearFollowSpeed * Time.deltaTime); //TEMP move towards target
-            velocity = newPosition - transform.position;                                                                   //Mark current velocity
-
-            //Cap velocity:
-            float currentSpeed = velocity.magnitude;                               //Get current speed
-            float effectiveMaxSpeed = maxSpeed * Time.deltaTime * scaleMultiplier; //Initialize variable to store modified maximum speed
-            if (currentSpeed > effectiveMaxSpeed) //Max velocity has been exceeded
+            //Rotate system:
+            Quaternion newRotation = Quaternion.Slerp(transform.rotation, controllerTarget.rotation, angularFollowSpeed * Time.deltaTime);
+            List<Vector3> prevFingerPositions = new List<Vector3>();
+            foreach (Transform finger in fingerTargets)
             {
-                velocity = effectiveMaxSpeed * velocity.normalized;                                                  //Clamp velocity to maximum
-                currentSpeed = effectiveMaxSpeed;                                                                    //Update current speed measurement
-                newPosition = Vector3.MoveTowards(transform.position, controllerTarget.position, effectiveMaxSpeed); //Limit motion to mirror clamped velocity
+                prevFingerPositions.Add(finger.position);
+            }
+            transform.rotation = newRotation; //Set new rotation
+            for (int i = 0; i < fingerTargets.Length; i++) //Iterate through fingerTargets array
+            {
+                Vector3 newFingerPos = GetObstructedPosition(prevFingerPositions[i], fingerTargets[i].position, fingerTipRadius * scaleMultiplier); //Keep each finger individually obstructed (scrubbing out velocity movement)
+                fingerTargets[i].position = newFingerPos;                                                                                           //Set new finger position
             }
 
+            //Modify position
+            Vector3 newPosition = Vector3.Lerp(transform.position, offsetTargetPos, linearFollowSpeed * Time.deltaTime);
+
             //Obstruct movement:
-            float scaledRadius = palmCollisionRadius * scaleMultiplier;                                                                      //Get working radius adjusted for player scale
-            Vector3 scaledOffset = palmColliderOffset * scaleMultiplier;                                                                     //Get working positional offset adjusted for player scale
-            Vector3 unobstructedPosition = newPosition;                                                                                      //Save position before obstruction
-            newPosition = GetObstructedPosition(transform.position + scaledOffset, newPosition + scaledOffset, scaledRadius) - scaledOffset; //Obstruct position
+            Vector3 unobstructedPosition = newPosition;                           //Save position before obstruction
+            float scaledRadius = palmCollisionRadius * scaleMultiplier;
+            newPosition = GetObstructedPosition(transform.position, newPosition, scaledRadius); //Obstruct position
 
             //Commit hand movement:
             transform.position = newPosition; //Set new position
 
             //Project & obstruct fingers:
-            obstructedTarget.position = GetObstructedPosition(obstructedTarget.position, controllerTarget.position, scaledRadius); //Update position of obstructed target
-            Vector3 projectionDepth = controllerTarget.position - obstructedTarget.position;                                       //Get amount by which fingers should be projected out based on obstruction
-            scaledRadius = fingerTipRadius * scaleMultiplier;                                                                      //Update scaled radius so it is useable for fingertips
+            obstructedTarget.position = GetObstructedPosition(obstructedTarget.position, offsetTargetPos, scaledRadius); //Update position of obstructed target
+            Vector3 projectionDepth = offsetTargetPos - obstructedTarget.position;                                       //Get amount by which fingers should be projected out based on obstruction
+            projectionDepth = Vector3.ClampMagnitude(projectionDepth, maxProjectionDepth);                               //Clamp projectionDepth magnitude
+            scaledRadius = fingerTipRadius * scaleMultiplier;                                                            //Update scaled radius so it is useable for fingertips
             for (int i = 0; i < fingerTargets.Length; i++) //Iterate through fingerTargets array
             {
                 Vector3 fingerIdealPos = fingerRoots[i].position;                                                                                             //Get ideal target relative to finger root
